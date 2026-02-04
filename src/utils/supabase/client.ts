0f78aae261996c28e697f83ac9e86b4c79518fb9
mediauqi <backupmediauqi@gmail.com>
2026-02-05 06:23:13 +0700

    Update client.ts

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "YOUR_SUPABASE_URL";
-const supabaseAnonKey = "YOUR_SUPABASE_ANON_KEY";
+const supabaseUrl = "YOUR_SUPABASE_URhttps://krchvhkgjbkxaudatzon.supabase.co";
+const supabaseAnonKey = "YeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtyY2h2aGtnamJreGF1ZGF0em9uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyMTIyMTQsImV4cCI6MjA4NTc4ODIxNH0.PiIRaxT83Kor-OWs0XIAmdlTeJxzXu8nEC-4G_JaePw";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
